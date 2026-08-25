e90bf01e5a6e21cb9776f01348137d0626079e05
a671271260 <a671271260@users.noreply.github.com>
2026-08-25 03:07:45 +0000

    feat: 读取当前分支
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- kejilion.sh
+++ kejilion.sh
@@ -19407,7 +19407,7 @@ while true; do
 	  22|uptime-kuma)
 		local app_id="22"
 		local docker_name="uptime-kuma"
-		local docker_img="louislam/uptime-kuma:latest"
+		local docker_img="louislam/uptime-kuma:2"
 		local docker_port=8022
 
 
@@ -19418,7 +19418,7 @@ while true; do
 				-p ${docker_port}:3001 \
 				-v /home/docker/uptime-kuma/uptime-kuma-data:/app/data \
 				--restart=always \
-				louislam/uptime-kuma:latest
+				louislam/uptime-kuma:2
 
 		}
 
